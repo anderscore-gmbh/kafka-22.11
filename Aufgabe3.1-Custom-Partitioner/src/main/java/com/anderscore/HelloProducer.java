5f4c63a15001fe0df30c98fb59cbce229d77b3db
Jan Lühr <jan.luehr@anderscore.com>
2022-10-04 21:09:31 +0200

    Added Aufgabe 3.1

--- Aufgabe3.1-Custom-Partitioner/src/main/java/com/anderscore/HelloProducer.java
+++ Aufgabe3.1-Custom-Partitioner/src/main/java/com/anderscore/HelloProducer.java
@@ -1,38 +1,31 @@
 package com.anderscore;
 
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 public class HelloProducer {
 
     private final Properties props = new Properties();
-    private final KafkaProducer<String, String> producer;
+    private final KafkaProducer<Integer, String> producer;
 
     public HelloProducer(){
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, MyPartitioner.class.getName());
         producer= new KafkaProducer<>(props);
     }
 
     public void produce() {
-        ProducerRecord<String,String> helloMsg = new ProducerRecord<>("HelloTopic","Hello Key", "Hello Value");
-
-
-        for (..) {
-            try {
-                producer.send(helloMsg).get();
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException(e);
-            }
-            producer.close();
-
+        for(int i=0; i<100; i++) {
+            ProducerRecord<Integer, String> Msg = new ProducerRecord<>("MyPartitionerTopic", i, "This is message #" + Integer.toString(i));
+            producer.send(Msg);
         }
-        // Wait
+        producer.close();
     }
 }
